04551917805dd4b77539c3c2abc23d559b8f3016
stanley2058 <stanley2058@yahoo.com.tw>
2021-04-18 16:14:06 +0800

    add vundle

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cp .alias_profile $HOME
 cp .env_profile $HOME
 cp .vimrc $HOME
@@ -6,4 +7,5 @@ cp -r .config $HOME/.config
 sed -i -e '$asource $HOME/.alias_profile\nsource $HOME/.env_profile' $HOME/.bashrc
 sed -i -e '$asource $HOME/.alias_profile\nsource $HOME/.env_profile' $HOME/.zshrc
 
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
