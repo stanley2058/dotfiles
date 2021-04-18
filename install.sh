559f72d8339f8bc883c41c0463345c1bdcd460c4
stanley2058 <stanley2058@yahoo.com.tw>
2021-04-18 16:20:43 +0800

    fix cp

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cp .alias_profile $HOME
 cp .env_profile $HOME
 cp .vimrc $HOME
-cp -r .config $HOME/.config
+cp -r .config $HOME
 
 sed -i -e '$asource $HOME/.alias_profile\nsource $HOME/.env_profile' $HOME/.bashrc
 sed -i -e '$asource $HOME/.alias_profile\nsource $HOME/.env_profile' $HOME/.zshrc
