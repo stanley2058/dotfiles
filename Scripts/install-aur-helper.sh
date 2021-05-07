88a0c9dfad1258adf0b5e1b240ebe4e1320e4df3
stanley2058 <stanley2058@yahoo.com.tw>
2021-05-07 17:14:46 +0800

    add install aur helper script

---
+++ Scripts/install-aur-helper.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed git base-devel
+
+if ! command -v yay &> /dev/null; then
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si
+    cd ..
+    rm -rf yay-bin
+fi
+
+if ! command -v paru &> /dev/null; then
+    git clone https://aur.archlinux.org/paru-bin.git
+    cd paru-bin
+    makepkg -si
+    cd ..
+    rm -rf paru-bin
+fi
