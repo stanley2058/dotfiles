ee88057daf619a9bbd95d180c2ca1222af09a735
stanley2058 <stanley2058@yahoo.com.tw>
2021-09-25 19:51:24 +0800

    add a template for server

---
+++ ArchInstall/install-os.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+echo "ArchLinux semi-automatic installation script"
+
+echo "Setting up pacman.conf"
+sed -i "s/#Parallel/Parallel/g; s/#Color/Color/g" /etc/pacman.conf
+
+printf "Hostname: "
+read -r NEW_HOSTNAME
+
+printf "Username: "
+read -r NEW_USER
+
+printf "Password: "
+read -rs PASSWD
+echo
+
+printf "Password again: "
+read -rs CONF_PASSWD
+echo
+
+if [ "$PASSWD" != "$CONF_PASSWD" ]; then
+    echo "Password not aligned!"
+    exit 1
+fi
+
+printf "Drive to format: "
+read -r DRIVE
+
+printf "Drive size: "
+read -r SIZE
+
+echo
+echo "[0] Server  - TTY"
+echo "[1] Desktop - KDE"
+printf "Select a profile to install [0-1]:"
+read -r PROFILE_TO_USE
+
+set TEMPLATE
+if [ "$PROFILE_TO_USE" == "0" ]; then
+    TEMPLATE=archinstall-server.template.json
+elif [ "$PROFILE_TO_USE" == "1" ]; then
+    TEMPLATE=archinstall-kde.template.json
+else
+    echo "Not supported profile"
+    exit 1
+fi
+
+sed "s/\$USER/$NEW_USER/g; s/\$PASSWORD/$PASSWD/g; s/\$HOSTNAME/$NEW_HOSTNAME/g; s|\$DRIVE|$DRIVE|g; s/\$SIZE/$SIZE/g" \
+    $TEMPLATE > archinstall.json
+
+echo
+printf "Modify the config? [y/N]: "
+read -r MODIFY
+
+if [ "$MODIFY" == "y" ] || [ "$MODIFY" == "Y" ] || [ "$MODIFY" == "yes" ] || [ "$MODIFY" == "Yes" ]; then
+    $EDITOR archinstall.json
+fi
+
+printf "Proceed to install? [Y/n]: "
+read -r RUN_INSTALL
+
+if [ "$RUN_INSTALL" == "n" ] || [ "$RUN_INSTALL" == "N" ] || [ "$RUN_INSTALL" == "no" ] || [ "$RUN_INSTALL" == "No" ]; then
+    echo "Exiting, nothing has been installed."
+    exit 1
+fi
+
+archinstall --config archinstall.json
