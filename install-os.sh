0f191e191f3d0b09781b170fa7b5a200b2ff84fb
stanley2058 <stanley2058@yahoo.com.tw>
2021-09-25 15:14:49 +0800

    add os installation script for archinstall

---
+++ install-os.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+printf "Hostname: "
+read NEW_HOSTNAME
+
+printf "Username: "
+read NEW_USER
+
+printf "Password: "
+read -s PASSWD
+echo
+
+printf "Password again: "
+read -s CONF_PASSWD
+echo
+
+if [ "$PASSWD" != "$CONF_PASSWD" ]; then
+    echo "Password not aligned!"
+    exit 1
+fi
+
+printf "Drive to format: "
+read DRIVE
+
+printf "Drive size: "
+read SIZE
+
+sed "s/\$USER/$NEW_HOSTNAME/g; s/\$PASSWORD/$PASSWD/g; s/\$HOSTNAME/$NEW_HOSTNAME/g; s/\$DRIVE/$DRIVE/g; s/\$SIZE/$SIZE/g" \
+    archinstall.template.json > archinstall.json
+
+echo
+echo "Please review the created config:"
+
+cat archinstall.json | awk '{print "\033[90m"$0}'
+echo -e "\033[0m"
+
+printf "Modify the config? [y/N]: "
+read MODIFY
+
+if [ "$MODIFY" == "Y" ] || [ "$MODIFY" == "y" ] || [ "$MODIFY" == "yes" ] || [ "$MODIFY" == "Yes" ]; then
+    $EDITOR archinstall.json
+fi
+
+printf "Proceed to install? [y/N]: "
+read RUN_INSTALL
+
+if [ "$RUN_INSTALL" == "Y" ] || [ "$RUN_INSTALL" == "y" ] || [ "$RUN_INSTALL" == "yes" ] || [ "$RUN_INSTALL" == "Yes" ]; then
+    archinstall --config archinstall.json
+else
+    echo "Exiting, nothing has been installed."
+    exit 1
+fi
