fa60022c37b93cdfec1f4eae0d04e8091fef1a17
stanley2058 <stanley2058@yahoo.com.tw>
2022-03-10 21:03:05 +0800

    feat: add icon for ssh session

--- Scripts/os-logo.sh
+++ Scripts/os-logo.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
 
 distro=Linux
+logo=""
+prefix=
 
 if [ -f "/etc/os-release" ]; then
     source /etc/os-release
     distro="$NAME"
 fi
 
+# check if is a ssh session
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+    prefix="\033[93m撚\033[0m"
+fi
+
 case "$distro" in
-    "Arch Linux") echo -e "\033[34m\033[0m"
+    "Arch Linux")   logo="\033[34m\033[0m"
         ;;
-    "Manjaro") echo -e "\033[32m\033[0m"
+    "Manjaro")      logo="\033[32m\033[0m"
         ;;
-    "Ubuntu") echo -e "\033[31m\033[0m"
+    "Ubuntu")       logo="\033[31m\033[0m"
         ;;
-    "Fedora") echo -e "\033[34m\033[0m"
+    "Fedora")       logo="\033[34m\033[0m"
         ;;
-    "Debian") echo -e "\033[32m\033[0m"
-        ;;
-    *) echo -e ""
+    "Debian")       logo="\033[32m\033[0m"
         ;;
 esac
 
+echo -e $prefix$logo
