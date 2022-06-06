f143f11ccc8e580dd0192dcd7e1bf4b6b22f5059
stanley2058 <stanley2058@yahoo.com.tw>
2022-06-06 16:07:18 +0800

    feat: make dep install script working on fedora

--- install-dep.sh
+++ install-dep.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-./Scripts/install-aur-helper.sh
-
-sudo pacman -S --noconfirm --needed git base-devel tmux gvim fish zsh fzf starship exa lsd ranger
-
-if command -v paru &> /dev/null; then 
-    paru -S --needed --noconfirm c-lolcat fisher
-elif command -v yay &> /dev/null; then
-    yay -S --needed --noconfirm c-lolcat fisher
+if [ -f "/etc/os-release" ]; then
+    source /etc/os-release
+    distro="$NAME"
 fi
+
+case "$distro" in
+    "Arch Linux")
+        ./Scripts/install-aur-helper.sh
+        sudo pacman -S --noconfirm --needed git base-devel tmux gvim fish zsh fzf starship exa lsd ranger neovim
+        if command -v paru &> /dev/null; then 
+            paru -S --needed --noconfirm c-lolcat fisher
+        elif command -v yay &> /dev/null; then
+            yay -S --needed --noconfirm c-lolcat fisher
+        fi
+        ;;
+    "Manjaro")
+        ./Scripts/install-aur-helper.sh 
+        sudo pacman -S --noconfirm --needed git base-devel tmux gvim fish zsh fzf starship exa lsd ranger neovim
+        if command -v paru &> /dev/null; then 
+            paru -S --needed --noconfirm c-lolcat fisher
+        elif command -v yay &> /dev/null; then
+            yay -S --needed --noconfirm c-lolcat fisher
+        fi
+        ;;
+    "Ubuntu")
+        ;;
+    "Debian")
+        ;;
+    "Fedora Linux")
+        sudo dnf install tmux gvim fish zsh fzf starship exa lsd ranger neovim lolcat
+        ;;
+esac
+
