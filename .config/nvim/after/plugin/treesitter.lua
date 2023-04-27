0093ba175b8d14719d2be78fd837558d53e0bcbf
stanley2058 <stanley2058@yahoo.com.tw>
2023-04-27 21:33:31 +0800

    fix: fix treesitter vimdoc

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "help", "c", "lua", "rust", "go", "vim", "javascript", "typescript", "java", "fish", "awk" },
+    ensure_installed = { "vimdoc", "c", "lua", "rust", "go", "vim", "javascript", "typescript", "java", "fish", "awk" },
 
     -- Install parsers synchornously (only applied to `ensure_installed`)
     sync_install = false,
