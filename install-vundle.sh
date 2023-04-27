cbabb7c58f40bb08f210fa01e466a718c4c3e179
stanley2058 <stanley2058@yahoo.com.tw>
2023-04-28 00:45:18 +0800

    Revert "fix: nvim packer boostrap"
    This reverts commit 1bef56bbfb1b31e8ed6fe7c08acaad989c50e1f0.

--- install-vundle.sh
+++ install-vundle.sh
@@ -2,8 +2,5 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 vim +PluginInstall +qall
-
-# this is not a bug, it's necessary to run it twice
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim +PackerSync +TSUpdateSync +qa
 
