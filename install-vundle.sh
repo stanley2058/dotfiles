1bef56bbfb1b31e8ed6fe7c08acaad989c50e1f0
stanley2058 <stanley2058@yahoo.com.tw>
2023-04-28 00:41:07 +0800

    fix: nvim packer boostrap

--- install-vundle.sh
+++ install-vundle.sh
@@ -2,5 +2,8 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 vim +PluginInstall +qall
-nvim +PackerSync +TSUpdateSync +qa
+
+# this is not a bug, it's necessary to run it twice
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
