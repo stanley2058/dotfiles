f450cb735b3fd0006a822b69f3a570be60628f6c
stanley2058 <stanley2058@yahoo.com.tw>
2024-01-17 11:47:41 +0800

    feat: add gitlinker

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,11 +4,36 @@
 
 local wk = require("which-key")
 
-wk.register({
+local commonMapping = {
     d = {
         '"_d',
         "Delete to void",
     },
+    g = {
+        -- browse
+        l = {
+            "<cmd>GitLink<cr>",
+            "Copy git permlink to clipboard",
+        },
+        L = {
+            "<cmd>GitLink!<cr>",
+            "Open git permlink in browser",
+        },
+        -- blame
+        b = {
+            "<cmd>GitLink blame<cr>",
+            "Copy git blame link to clipboard",
+        },
+        B = {
+            "<cmd>GitLink! blame<cr>",
+            "Open git blame link in browser",
+        },
+    },
+}
+
+wk.register(commonMapping, { prefix = "<leader>" })
+wk.register(commonMapping, { prefix = "<leader>", mode = "v" })
+wk.register({
     x = {
         ["+"] = {
             "<cmd>!chmod +x %<CR>",
@@ -16,9 +41,3 @@ wk.register({
         },
     },
 }, { prefix = "<leader>" })
-wk.register({
-    d = {
-        '"_d',
-        "Delete to void",
-    },
-}, { prefix = "<leader>", mode = "v" })
